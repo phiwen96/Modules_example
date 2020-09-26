74b783f8d87757aa1bd7dcb77f1b5f7c6e4171c2
Philip Wenkel <philip.sve@hotmail.com>
2020-09-26 08:44:14 +0200

    yo

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+#include <iostream>
+using namespace std;
+
+
+import math;
+
+int main(){
+   
+   cout << add(2000, 20) << endl;
+   
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+clang++ -std=c++2a -fmodules-ts --precompile math.cppm -o math.pcm && clang++ -std=c++2a -fmodules-ts -c math.pcm -o math.o && clang++ -std=c++2a -fmodules-ts -fprebuilt-module-path=. math.o main.cpp -o math && ./math
